6216e5a00d248151bca1579f51ed0f335e1b9f55
dangerhesae <112620076+dangerhesae@users.noreply.github.com>
2023-06-06 03:31:38 +0530

    dynamic route
    added dynamic route using product id
    add and access product id 
    access product using product id
    added add to cart and details buttn

--- product.js
+++ product.js
@@ -7,23 +7,27 @@ const p = path.join(
   'products.json'
 );
 
-const getProductsFromFile = cb => {
+const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-      cb([]);
+       cb([]);
     } else {
-      cb(JSON.parse(fileContent));
+    cb(JSON.parse(fileContent));
     }
   });
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductsFromFile(products => {
+    this.id=Math.random().toString();
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
@@ -32,6 +36,14 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    getProductsFromFile(cb);
+    getProductsFromFile(cb)
+  }
+  static findById(id,cb)
+  {
+    getProductsFromFile(products=>
+      {
+        const product=products.find(p=>p.id===id);
+        cb(product);
+      })
   }
 };
